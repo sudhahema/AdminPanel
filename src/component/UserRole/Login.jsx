03278c777676c04b192fdfd4be8199998fcf8177
hemasudha <hemasudhashini@cloudrevelinnovation.com>
2024-02-29 16:04:28 +0530

    login and register page changes added

--- src/component/UserRole/Login.jsx
+++ src/component/UserRole/Login.jsx
@@ -1,74 +1,73 @@
-
-import React, {useState} from "react";
-import { TextField, Button } from "@mui/material";
+import React, { useState } from "react";
+// import { TextField, Button } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
-import loginbg from '../../assests/image/loginbg.jpg'
- 
+import axios from "axios";
+
+
 const Login = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const navigate = useNavigate()
-    // const [emailError, setEmailError] = useState(false)
-    // const [passwordError, setPasswordError] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-
-  const handleSubmit =  (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    axios.get('https://65a8d210219bfa371867b1d0.mockapi.io/api/fakeapi')
-    .then(response => {
-      console.log(response.data);
-      const loginData = response.data.find(user => user.email === email );
-      if (loginData) {
-        console.log("logged in")
-        navigate('/admin')
-        
-
-      } else {
-        console.log("User not found");
-      }
-    })
-    .catch(error => {
-      console.error("An error occurred:", error);
-    });
+    axios
+      .get("https://65a8d210219bfa371867b1d0.mockapi.io/api/fakeapi")
+      .then((response) => {
+        console.log(response.data);
+        const loginData = response.data.find((user) => user.email === email);
+        if (loginData) {
+          console.log("logged in");
+          navigate("/admin");
+        } else {
+          console.log("User not found");
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
   };
-    return ( 
-        <React.Fragment>
-          <div className="align_page">
-        <form autoComplete="off" onSubmit={handleSubmit} className="form_container">
-            <h2>Login Form</h2>
-                <TextField 
-                    label="Email"
-                    onChange={e => setEmail(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="email"
-                    sx={{mb: 3}}
-                    fullWidth
-                    value={email}
-                    // error={emailError}
-                 />
-                 <TextField 
-                    label="Password"
-                    onChange={e => setPassword(e.target.value)}
-                    required
-                    variant="outlined"
-                    color="secondary"
-                    type="password"
-                    value={password}
-                    // error={passwordError}
-                    fullWidth
-                    sx={{mb: 3}}
-                 />
-                 <Button variant="outlined" color="secondary" type="submit">Login</Button>
-             
-        </form>
-      
-        <medium>Need an account? <Link to="/">Register here</Link></medium>
+  return (
+    <React.Fragment>
+      <div className="bodycontainer">
+        <div className="container">
+          <div className="login-box">
+            <h2>Login</h2>
+            <form autoComplete="off" onSubmit={handleSubmit}>
+              <div className="input-box">
+                <input
+                  type="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label>Email</label>
+              </div>
+              <div className="input-box">
+                <input
+                  type="password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label>Password</label>
+              </div>
+              <div className="forgot-pass">
+                <a href="#">Forgot your password?</a>
+              </div>
+              <button type="submit" className="btnlogin">
+                Login
+              </button>
+              <div className="signup-link">
+                <Link to="/">Register here</Link>
+              </div>
+            </form>
+          </div>
+          {[...Array(50)].map((_, i) => (
+            <span key={i} style={{ "--i": i }}></span>
+          ))}
         </div>
-        </React.Fragment>
-     );
-}
- 
+      </div>
+    </React.Fragment>
+  );
+};
+
 export default Login;
